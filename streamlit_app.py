5772bbf2d3bf71347fc5ea64e760446c4a4dc64b
Michelle Gracie <49012767+mgracie@users.noreply.github.com>
2023-01-12 14:44:02 +0800

    Update streamlit_app.py
    Add additional text formatted as header

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,5 @@ streamlit.text('\N{bowl with spoon} Omega 3 & Blueberry Oatmeal')
 streamlit.text('\N{leafy green} Kale, Spinach and Rocket Smoothie')
 streamlit.text('\N{egg} Hard-boiled Egg on Wholewheat Toast')
 streamlit.text('\N{avocado} Smashed Avocado on Toast')
+
+streamlit.header('\N{banana} \N{grapes} Build your own fruit smoothies \N{blueberries} \{kiwi fruit}')
