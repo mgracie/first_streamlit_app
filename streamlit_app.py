7e92557e532cb681bce08602fd377282c693f26d
Michelle Gracie <49012767+mgracie@users.noreply.github.com>
2023-01-12 14:28:47 +0800

    Update streamlit_app.py
    testing emoji

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit
 streamlit.title('New Parents New Health Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('\N{bowl with spoon} Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach and Rocket Smoothie')
 streamlit.text('Hard-boiled Egg on Wholewheat Toast')
+streamlit.text('Smashed Avocado on Toast')
